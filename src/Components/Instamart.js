1e1ef043db4798973a22a24f40e2c5fd45be56de
akshay77 <ajakshayjain11@gmail.com>
2023-10-05 23:32:23 +0530

    added

--- src/Components/Instamart.js
+++ src/Components/Instamart.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const Section = ({ title, decscription }) => {
+
+  const[Isvisible,setIsvisible]=useState(false)
+  return (
+    <div className=" border-2 rounded-md mx-2 border-black mt-4">
+      <h1 className="font-bold">{title} </h1>
+      {Isvisible ? (
+        <button onClick={() => setIsvisible(false)}>hide</button>
+      ) : (
+        <button onClick={() => setIsvisible(true)}>show</button>
+      )}
+      {Isvisible && <p className="text-red-600">{decscription}</p>}
+    </div>
+  );
+};
 
 const Instamart = () => {
   return (
-    <div>Instamart</div>
-  )
-}
+    <>
+      <Section title={"Heading 1"} decscription={"this is heading 1"} />
+      <Section title={"Heading 2"} decscription={"this is heading 2"} />
+      <Section title={"Heading 3"} decscription={"this is heading 3"} />
+    </>
+  );
+};
 
-export default Instamart
+export default Instamart;
